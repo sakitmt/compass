76243fa88f1bd4cf84e3eaed77066c081f6f9248
瀧澤紗樹 <>
2024-05-16 01:31:29 +0900

    修正　2024/05/16

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -18,12 +18,8 @@
     <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
       <div class="w-25 vh-75 border p-3">
         <div class="register_form">
-          <div class="mt-3">
-            @if ($errors->all())<div class="error">
-              <li>{{$errors->first('over_name')}}</li>
-              <li>{{$errors->first('under_name')}}</li></div>
-            @endif
-            <div class="d-flex" style="justify-content:space-between">
+          <div class="">
+            <div class="d-flex mt-3" style="justify-content:space-between">
               <div class="" style="width:140px">
                 <label class="d-block m-0" style="font-size:13px">姓</label>
                 <div class="border-bottom border-primary" style="width:140px;">
@@ -37,13 +33,14 @@
                 </div>
               </div>
             </div>
+            @if ($errors->all())<div class="error">
+              <li>{{$errors->first('over_name')}}</li>
+              <li>{{$errors->first('under_name')}}</li></div>
+            @endif
           </div>
-          <div class="mt-3">
-              @if ($errors->all())<div class="error">
-                <li>{{$errors->first('over_name_kana')}}</li>
-                <li>{{$errors->first('under_name_kana')}}</li></div>
-              @endif
-            <div class="d-flex" style="justify-content:space-between">
+          <div class="">
+              
+            <div class="d-flex mt-3" style="justify-content:space-between">
               <div class="" style="width:140px">
                 <label class="d-block m-0" style="font-size:13px">セイ</label>
                 <div class="border-bottom border-primary" style="width:140px;">
@@ -57,34 +54,36 @@
                 </div>
               </div>
             </div>
+            @if ($errors->all())<div class="error">
+                <li>{{$errors->first('over_name_kana')}}</li>
+                <li>{{$errors->first('under_name_kana')}}</li></div>
+              @endif
           </div>
           <div class="mt-3">
-            @if ($errors->all())<div class="error">
-              <li>{{$errors->first('mail_address')}}</li></div>
-            @endif
+
             <label class="m-0 d-block" style="font-size:13px">メールアドレス</label>
             <div class="border-bottom border-primary">
               <input type="mail" class="w-100 border-0 mail_address" name="mail_address">
             </div>
+            @if ($errors->all())<div class="error">
+              <li>{{$errors->first('mail_address')}}</li></div>
+            @endif
           </div>
         </div>
         <div class="mt-3">
-          @if ($errors->all())<div class="error">
-            <li>{{$errors->first('sex')}}</li></div>
-          @endif
+          
           <input type="radio" name="sex" class="sex" value="1">
           <label style="font-size:13px">男性</label>
           <input type="radio" name="sex" class="sex" value="2">
           <label style="font-size:13px">女性</label>
           <input type="radio" name="sex" class="sex" value="3">
           <label style="font-size:13px">その他</label>
+          @if ($errors->all())<div class="error">
+            <li>{{$errors->first('sex')}}</li></div>
+          @endif
         </div>
         <div class="mt-3">
-          @if ($errors->all())<div class="error">
-            <li>{{$errors->first('old_year')}}</li>
-            <li>{{$errors->first('old_month')}}</li>
-            <li>{{$errors->first('old_day')}}</li></div>
-          @endif
+          
           <label class="d-block m-0 aa" style="font-size:13px">生年月日</label>
           <select class="old_year" name="old_year">
             <option value="none">-----</option>
@@ -167,11 +166,14 @@
             <option value="31">31</option>
           </select>
           <label style="font-size:13px">日</label>
+          @if ($errors->all())<div class="error">
+            <li>{{$errors->first('old_year')}}</li>
+            <li>{{$errors->first('old_month')}}</li>
+            <li>{{$errors->first('old_day')}}</li></div>
+          @endif
         </div>
         <div class="mt-3">
-          @if ($errors->all())<div class="error">
-            <li>{{$errors->first('role')}}</li></div>
-          @endif
+          
           <label class="d-block m-0" style="font-size:13px">役職</label>
           <input type="radio" name="role" class="admin_role role" value="1">
           <label style="font-size:13px">教師(国語)</label>
@@ -181,11 +183,12 @@
           <label style="font-size:13px">教師(英語)</label>
           <input type="radio" name="role" class="other_role role" value="4">
           <label style="font-size:13px" class="other_role">生徒</label>
+          @if ($errors->all())<div class="error">
+            <li>{{$errors->first('role')}}</li></div>
+          @endif
         </div>
         <div class="select_teacher d-none">
-          @if ($errors->all())<div class="error">
-            <li>{{$errors->first('subject')}}</li></div>
-          @endif
+          
           <label class="d-block m-0" style="font-size:13px">選択科目</label>
           @foreach($subjects as $subject)
           <div class="">
@@ -193,21 +196,26 @@
             <label>{{ $subject->subject }}</label>
           </div>
           @endforeach
+          @if ($errors->all())<div class="error">
+            <li>{{$errors->first('subject')}}</li></div>
+          @endif
         </div>
-        <div class="mt-3">
+        <div>
+          <div class="mt-3">
+            <label class="d-block m-0" style="font-size:13px">パスワード</label>
+            <div class="border-bottom border-primary">
+              <input type="password" class="border-0 w-100 password" name="password">
+            </div>
+          </div>
+          <div class="mt-3">
+            <label class="d-block m-0" style="font-size:13px">確認用パスワード</label>
+            <div class="border-bottom border-primary">
+              <input type="password" class="border-0 w-100 password_confirmation" name="password">
+            </div>
+          </div>
           @if ($errors->all())<div class="error">
             <li>{{$errors->first('password')}}</li></div>
           @endif
-          <label class="d-block m-0" style="font-size:13px">パスワード</label>
-          <div class="border-bottom border-primary">
-            <input type="password" class="border-0 w-100 password" name="password">
-          </div>
-        </div>
-        <div class="mt-3">
-          <label class="d-block m-0" style="font-size:13px">確認用パスワード</label>
-          <div class="border-bottom border-primary">
-            <input type="password" class="border-0 w-100 password_confirmation" name="password">
-          </div>
         </div>
         <div class="mt-5 text-right">
           <input type="submit" class="btn btn-primary register_btn" disabled value="新規登録" onclick="return confirm('登録してよろしいですか？')">
